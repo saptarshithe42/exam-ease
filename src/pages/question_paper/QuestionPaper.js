e3a4fe26d5d46e9f056ada9304133b57eed604d9
Saptarshi Mondal <saptarshi.the.42@gmail.com>
2023-02-25 01:23:39 +0530

    print to pdf feature added

--- src/pages/question_paper/QuestionPaper.js
+++ src/pages/question_paper/QuestionPaper.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 import { useAuthContext } from '../../hooks/useAuthContext';
-import {useDocument} from "../../hooks/useDocument"
+import { useDocument } from "../../hooks/useDocument"
 import QuestionViewComponent from './QuestionViewComponent';
+import { useReactToPrint } from 'react-to-print';
+import ReactToPrint from 'react-to-print';
 
 import "./QuestionPaper.css"
 
@@ -14,19 +17,32 @@ function QuestionPaper() {
 
   const { document: questionPaper, error } = useDocument("question_papers", id)
 
+  const componentRef = useRef()
+
+  const handlePrint = useReactToPrint({
+    documentTitle: `question-paper-${id}`,
+    content: () => componentRef.current,
+  })
+
+
 
   return (
-    <div className="question-paper">
+    <div className="question-paper" >
+      <button onClick={handlePrint}>Print Question Paper</button>
+
       {questionPaper &&
-        <div>
-          <h1>{questionPaper.name}</h1>
+        <div style={{textAlign : "center"}} ref={componentRef}>
+          <h1 style={{marginTop : "2rem"}}>{questionPaper.name}</h1>
           <h5>Created By : {user.displayName}</h5>
+
+          {/* {!printPressed && <button onClick={printHandler}>Print Question Paper</button>} */}
+
           {/* <div className="question-box"> */}
           {questionPaper.questionsList.map((question) => {
-              return (<div key={question.qno} className="question-box"> <QuestionViewComponent question={question} /> </div>)
+            return (<div key={question.qno} className="question-box"> <QuestionViewComponent question={question} /> </div>)
           })}
           {/* </div> */}
-          
+
         </div>
       }
     </div>
--- src/pages/question_paper/QuestionViewComponent.js
+++ src/pages/question_paper/QuestionViewComponent.js
@@ -2,15 +2,35 @@ import React from 'react'
 
 import "./QuestionViewComponent.css"
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 function QuestionViewComponent({ question }) {
+
+  const options = shuffle(question.options)
 
 
   return (
     <div className="question-view">
 
-      <div className="question">{question.question}</div>
+      <div className="question">{question.qno}) {question.question}</div>
       <div className="options-list">
-        {question.options.map((option, index) => {
+        {options.map((option, index) => {
           return (<span className="options" key={index}>({index + 1}) {option}</span>) 
         })}
       </div>
